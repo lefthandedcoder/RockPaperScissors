dc9101d8918fbfc97fd835407f9b6f1a392542ec
Christian Dye <christiancdye@gmail.com>
2022-03-03 13:59:51 -0600

    trying to add buttons

--- main.js
+++ main.js
@@ -1,65 +1,78 @@
+const statsBar = document.getElementById("stats").innerHTML;
+const roundResults = document.getElementById("round-result").innerHTML;
+let user = 0;
+let pc = 0;
+let ties = 0;
+const win = 'You win! ';
+const lose = 'You lose! ';
+const tie = 'Tie!';
+const rbs = 'Rock beats Scissors';
+const sbp = 'Scissors beats Paper';
+const pbr = 'Paper beats Rock';
+const error = 'Uh oh... Something\'s not right';
+
 // Function to randomly return either 'Rock,' 'Paper,' or 'Scissors'
 function computerPlay(){
     const num = Math.floor(Math.random() * 3);
-    if (num === 0) return 'Rock';
-    else if (num === 1) return 'Paper';
-    else if (num === 3) return 'Scissors';
+    if (num === 0) return 'rock';
+    else if (num === 1) return 'paper';
+    else if (num === 3) return 'scissors';
     else return computerPlay();
 }
 
+function loseRound(expl) {
+    roundResults = lose + expl;
+    pc++;
+    stats(pc, user, ties);
+}
+
+function winRound(expl) {
+    roundResults = win + expl;
+    user++;
+    stats(pc, user, ties);
+}
+
+function tie() {
+    roundResults = tie;
+    tie++;
+    stats(pc, user, ties);
+}
 
 // Plays a single round of rps, compares user response to computer play
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
     if (playerSelection == 'rock') {
-        if (computerSelection == 'scissors') return 'You win! Rock beats Scissors';
-        else if (computerSelection == 'rock') return 'Tie!';
-        else if (computerSelection == 'paper') return 'You lose! Paper beats Rock';
-        else return 'Uh oh... Something\'s not right';
-    }
-    else if (playerSelection == 'paper') {
-        if (computerSelection == 'rock') return 'You win! Paper beats Rock';
-        else if (computerSelection == 'paper') return 'Tie!';
-        else if (computerSelection == 'scissors') return 'You lose! Scissors beats Paper';
-        else return 'Uh oh... Something\'s not right';
-    }
-    else if (playerSelection == 'scissors') {
-        if (computerSelection == 'paper') return 'You win! Scissors beats Paper';
-        else if (computerSelection == 'scissors') return 'Tie!';
-        else if (computerSelection == 'rock') return 'You lose! Rock beats Scissors';
-        else return 'Uh oh... Something\'s not right';
-    }
-    else return 'Uh oh... Something\'s not right';
+        if (computerSelection == 'scissors'){
+            winRound(rbs);
+        } else if (computerSelection == 'rock') {
+            tie();
+        } else if (computerSelection == 'paper'){
+            loseRound(pbr);
+        } else roundResults = error;
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'rock'){
+            winRound(pbr);
+        } else if (computerSelection == 'paper') {
+            tie();
+        } else if (computerSelection == 'scissors'){
+            loseRound(sbp);
+        } else roundResults = error;
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'paper'){
+            winRound(sbp);
+        } else if (computerSelection == 'scissors') {
+            tie();
+        } else if (computerSelection == 'rock'){
+            loseRound(rbs);
+        } else roundResults = error;
+    } else roundResults = error;
+}
+
+function stats(pc, user, tie){
+    prompt("Stats: PC: " + pc + " | User: " + user + " | Ties: " + tie);
 }
 
 // Actual game, uses while loop until someone wins
-function game(){
-    let user = 0;
-    let pc = 0;
-    let tie = 0;
-    const click = " Click OK!"; 
-    let result = "";
-    let start = prompt("Let's play!" + click);
-    while (pc < 3 || user < 3) { 
-        let playerSelection = prompt("Type 'rock', 'paper', or 'scissors'");
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection)
-        prompt(result + click);
-        if (result.includes('win')) user++;
-        else if (result.includes('lose')) pc++;
-        else tie++;
-        if (user > 2) {
-            result = "Congratulations! You won!";
-            prompt(result + click);
-            console.log("Stats: PC: " + pc + " | User: " + user + " | Ties: " + tie);
-            break;
-        } else if (pc > 2) {
-            result = "Gameover! You lost!";
-            prompt(result + click);
-            console.log("Stats: PC: " + pc + " | User: " + user + " | Ties: " + tie);
-            break;
-        } else continue;
-    }
+function game(playerSelection){
+    let computerSelection = computerPlay();
+    return playRound(playerSelection, computerSelection);
 }
-
