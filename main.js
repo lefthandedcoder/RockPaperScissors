099993d6a87a925fbb38f7c40cfbdf657c9eaf15
Christian Dye <christiancdye@gmail.com>
2022-03-18 21:29:24 -0500

    adding gui to rps

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+const roundResult = document.getElementById('round-result');
+const gameEnd = document.getElementById('game-result');
+const pcScore = document.getElementById('pc-score');
+const playerScore = document.getElementById('player-score');
+const playerImg = document.getElementById('player-img');
+const pcImg = document.getElementById('pc-img');
+const rockImg = "images\\rock.png";
+const scissorsImg = "images\\scissors.png";
+const paperImg = "images\\paper-plane.png";
 let user = 0;
 let pc = 0;
 let ties = 0;
@@ -17,24 +26,27 @@ function computerPlay(){
     const num = Math.floor(Math.random() * 3);
     if (num === 0) return 'rock';
     else if (num === 1) return 'paper';
-    else if (num === 3) return 'scissors';
+    else if (num === 2) return 'scissors';
     else return computerPlay();
 }
 
 function loseRound(expl) {
-    document.getElementById('round-result').innerHTML = lose + expl;
+    roundResult.style.color = "red";
+    roundResult.innerHTML = lose + expl;
     pc++;
     stats(pc, user, ties);
 }
 
 function winRound(expl) {
-    document.getElementById('round-result').innerHTML = win + expl;
+    roundResult.style.color = "green";
+    roundResult.innerHTML = win + expl;
     user++;
     stats(pc, user, ties);
 }
 
 function tieRound() {
-    document.getElementById('round-result').innerHTML = tie;
+    roundResult.style.color = "black";
+    roundResult.innerHTML = tie;
     ties++;
     stats(pc, user, ties);
 }
@@ -42,61 +54,97 @@ function tieRound() {
 // Plays a single round of rps, compares user response to computer play
 function playRound(playerSelection, computerSelection){
     if (playerSelection == 'rock') {
+        playerImg.src = rockImg;
         if (computerSelection == 'scissors'){
+            pcImg.src = scissorsImg;
             winRound(rbs);
         } else if (computerSelection == 'rock') {
+            pcImg.src = rockImg;
             tieRound();
         } else if (computerSelection == 'paper'){
+            pcImg.src = paperImg;
             loseRound(pbr);
-        } else roundResults = error;
+        } else roundResult = error;
     } else if (playerSelection == 'paper') {
+        playerImg.src = paperImg;
         if (computerSelection == 'rock'){
+            pcImg.src = rockImg;
             winRound(pbr);
         } else if (computerSelection == 'paper') {
             tieRound();
         } else if (computerSelection == 'scissors'){
+            pcImg.src = scissorsImg;
             loseRound(sbp);
-        } else roundResults = error;
+        } else roundResult = error;
     } else if (playerSelection == 'scissors') {
+        playerImg.src = scissorsImg;
         if (computerSelection == 'paper'){
+            pcImg.src = paperImg;
             winRound(sbp);
         } else if (computerSelection == 'scissors') {
+            pcImg.src = scissorsImg;
             tieRound();
         } else if (computerSelection == 'rock'){
+            pcImg.src = rockImg;
             loseRound(rbs);
-        } else roundResults = error;
-    } else roundResults = error;
+        } else roundResult = error;
+    } else roundResult = error;
 }
 
 function display(){
-    document.getElementById('stats').style.display = 'block';
     document.getElementById('round-result').style.display = 'block';
+    document.getElementById('game-result').style.display = 'none';
 }
 
-function stats(pc, user, tie){
-    document.getElementById('stats').innerHTML = "Stats: PC: " + pc + " | User: " + user + " | Ties: " + tie;
+function stats(pc, user){
+    playerScore.innerHTML = user;
+    pcScore.innerHTML = pc;
 }
 
 function gameResult(final){
-    document.getElementById('game-result').innerHTML = final;
+    gameEnd.innerHTML = final;
     pc = 0;
     user = 0;
     ties = 0;
     rock.style.display = 'none';
     paper.style.display = 'none';
     scissors.style.display = 'none';
+    roundResult.style.display = 'none';
+    document.getElementById('reset').style.gap = 0;
     document.getElementById('round-result').style.display = 'none';
+    document.getElementById('screen').style.display = 'none';
+    document.getElementById('game-result').style.fontSize = "large";
 }
 
 // Actual game, uses while loop until someone wins
 function game(playerSelection){
     let computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
-    if (pc > 2) gameResult('Gameover! You lose!');
-    else if (user > 2) gameResult('Congrats! You win!');
+    if (pc > 2) {
+        gameResult('Gameover! You lose!');
+        document.getElementById('game-result').style.display = 'block';
+        document.getElementById('player').style.display = 'none';
+        document.getElementById('pc').style.display = 'block';
+        document.getElementById('pc-score').style.display = 'none';
+        pcImg.src = "images\\ai.png";
+        pcImg.style.maxWidth = '10vw';
+        document.getElementById('screen').style.display = 'block';
+        document.getElementById('game-result').style.color = "red";
+    }
+    else if (user > 2) {
+        gameResult('Congrats! You win!');
+        document.getElementById('game-result').style.display = 'block';
+        document.getElementById('player').style.display = 'block';
+        playerImg.src = "images\\headband.png";
+        playerImg.style.maxWidth = '10vw';
+        document.getElementById('player-score').style.display = 'none';
+        document.getElementById('pc').style.display = 'none';
+        document.getElementById('screen').style.display = 'block';
+        document.getElementById('game-result').style.color = "green";
+    }
 };
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelector('#buttons').querySelectorAll('img');
 
 buttons.forEach((button) => button.addEventListener('click', () => {
     display();
